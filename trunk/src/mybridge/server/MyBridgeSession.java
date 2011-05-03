cca831b4a050186b6d245149b1224e51dc9c29fc
quanwei888@gmail.com <quanwei888@gmail.com@e3721f1a-ff26-9910-06c6-39595f2b65a3>
2011-05-03 00:18:27 +0000

    upup

--- trunk/src/mybridge/server/MyBridgeSession.java
+++ trunk/src/mybridge/server/MyBridgeSession.java
@@ -4,13 +4,18 @@ import mybridge.protocal.*;
 import xnet.core.server.Session;
 import xnet.core.util.IOBuffer;
 
+/**
+ * 实现mysql协议的session
+ * @author quanwei
+ *
+ */
 public class MyBridgeSession extends Session {
 
-	static int READ_HEADER = 0;
-	static int READ_BODY = 1;
+	static int READ_HEADER = 0;//正在读packet头，读操作时有效
+	static int READ_BODY = 1;//正在读packet body，读操作时有效
 
-	SessionHandle handle;
-	int currentState = READ_HEADER;
+	SessionHandle handle;//读完或写完1个packet后的协议处理器
+	int currentState = READ_HEADER;//初始状态为读packet头
 
 	@Override
 	public void complateRead(IOBuffer readBuf, IOBuffer writeBuf) throws Exception {
@@ -36,21 +41,5 @@ public class MyBridgeSession extends Session {
 	public void open(IOBuffer readBuf, IOBuffer writeBuf) throws Exception {
 		handle = new SessionHandle(this);
 		handle.onPacketInit(readBuf, writeBuf);
-	}
-
-	@Override
-	public void timeout(IOBuffer readBuf, IOBuffer writeBuf) throws Exception {
-	}
-
-	@Override
-	public void complateReadOnce(IOBuffer readBuf, IOBuffer writeBuf) throws Exception {
-	}
-
-	@Override
-	public void complateWriteOnce(IOBuffer readBuf, IOBuffer writeBuf) throws Exception {
-	}
-
-	@Override
-	public void close() {
 	}
 }
