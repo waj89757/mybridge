5bf24213e796acb23457e678d4b31e46aa4278ff
quanwei888@gmail.com <quanwei888@gmail.com@e3721f1a-ff26-9910-06c6-39595f2b65a3>
2011-05-03 14:51:52 +0000

    1

---
+++ trunk/src/mybridge/storyge/memcache/MemcacheHandle.java
@@ -0,0 +1,16 @@
+package mybridge.storyge.memcache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import mybridge.protocal.packet.Packet;
+import mybridge.protocal.packet.PacketCommand;
+import mybridge.storyge.Handle;
+
+public class MemcacheHandle implements Handle {
+	public List<Packet> doCommand(PacketCommand cmd) throws Exception {
+		List<Packet> packetList = new ArrayList<Packet>();
+		return packetList;
+	}
+}
