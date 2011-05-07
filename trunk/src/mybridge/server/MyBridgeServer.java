dd323eafde2c1150f6b39540bc5763fb9c78f4e8
quanwei888@gmail.com <quanwei888@gmail.com@e3721f1a-ff26-9910-06c6-39595f2b65a3>
2011-05-07 14:02:11 +0000

    upupup

--- trunk/src/mybridge/server/MyBridgeServer.java
+++ trunk/src/mybridge/server/MyBridgeServer.java
@@ -1,7 +1,7 @@
 package mybridge.server;
 
+import mybridge.engine.DefaultEngine;
 import mybridge.protocal.impl.Protocal;
-import mybridge.storyge.DefaultCmdManager;
 
 import org.apache.log4j.PropertyConfigurator;
 import xnet.core.server.*;
@@ -16,7 +16,7 @@ public class MyBridgeServer {
 		Config config = new Config();
 		config.session = MyBridgeSession.class;
 		PropertyConfigurator.configure("./conf/log4j.properties");
-		config.threadNum = 1;
+		config.threadNum = 4;
 		config.port = 10000;
 		config.rTimeout = 0;
 		config.wTimeout = 0;
@@ -24,7 +24,7 @@ public class MyBridgeServer {
 		config.keepalive = true;
 		config.maxConnection = 1000;
 		Server server = new Server(config);
-		Protocal.handleClass = DefaultCmdManager.class;
+		Protocal.handleClass = DefaultEngine.class;
 		server.run();
 	}
 
